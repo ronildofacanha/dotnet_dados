882594f59eff2205f31ea251a6616893d6348208
ronildofacanha <ronildofcjunior@gmail.com>
2021-08-27 22:03:51 -0400

    vai

---
+++ base/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
